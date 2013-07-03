82e34c2cd68552b73f9f602c62dde38ffb7a557a
Cyberixxx <cyberixny@gmail.com>
2013-07-03 12:14:16 +0300

    - removed static <i></i> block, no it is used only if class menu is defined

--- template/html/mod_menu/default_component.php
+++ template/html/mod_menu/default_component.php
@@ -29,18 +29,23 @@ if ($menu_icon = $item->params->get('menu_icon'))
 	$linktype = '<i class="' . $menu_icon . '"></i> ' . $linktype;
 }
 
+if ($menu_imageclass = $item->params->get('menu_imageclass'))
+{
+	$linktype = $linktype . '<i></i>';
+}
+
 switch ($item->browserNav):
 	default:
 	case 0:
-?><a<?php echo $class; ?> href="<?php echo $item->flink; ?>"<?php echo $title; ?>><?php echo $linktype; ?></a><i></i><?php
+?><a<?php echo $class; ?> href="<?php echo $item->flink; ?>"<?php echo $title; ?>><?php echo $linktype; ?></a><?php
 		break;
 	case 1:
 		// _blank
-?><a<?php echo $class; ?> href="<?php echo $item->flink; ?>" target="_blank"<?php echo $title; ?>><?php echo $linktype; ?></a><i></i><?php
+?><a<?php echo $class; ?> href="<?php echo $item->flink; ?>" target="_blank"<?php echo $title; ?>><?php echo $linktype; ?></a><?php
 		break;
 	case 2:
 	// Window.open
-?><a<?php echo $class; ?> href="<?php echo $item->flink; ?>" onclick="window.open(this.href,'targetWindow','toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes');return false;"<?php echo $title; ?>><?php echo $linktype; ?></a><i></i>
+?><a<?php echo $class; ?> href="<?php echo $item->flink; ?>" onclick="window.open(this.href,'targetWindow','toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes');return false;"<?php echo $title; ?>><?php echo $linktype; ?></a>
 <?php
 		break;
 endswitch;
